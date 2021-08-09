ae23a293538ab93ae5965452f9b8abc0de3997ae
toddhricik <toddhricik@gmail.com>
2021-08-08 23:12:14 -0400

    Listing 14.11

---
+++ Day14/putchar2.c
@@ -0,0 +1,18 @@
+/* Listing 14.11 - Demonstrates using putchar() to display strings. */
+#include <stdio.h>
+#define MAXSTRING 80
+char message[] = "Displayed with putchar().";
+int main(void){
+	int count;
+	for(count=0; count<MAXSTRING; count++){
+		/* Look for the end of the string. When it's found, */
+		/* write a newline character and exit the loop. */
+		if(message[count]==0){
+			putchar('\n');
+			break;
+		}
+		else /* If end of string not found, write the next charachter to screen. */
+		putchar(message[count]);
+	}
+	return 0;
+}
