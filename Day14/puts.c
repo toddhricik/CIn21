14e2e40cd94b0967d1614b1878eb6696efd350ac
toddhricik <toddhricik@gmail.com>
2021-08-08 23:18:27 -0400

    Listing 14.12

---
+++ Day14/puts.c
@@ -0,0 +1,11 @@
+/* Listing 14.12 - Demonstrates the puts() function */
+#include <stdio.h>
+/* Declare and initialize an array of pointers . */
+char *messages[5] = { "This", "is", "a", "short", "message." };
+int main(void){
+	int x;
+	for(x=0; x<5; x++)
+		puts(messages[x]);
+	puts("And this is the end!");
+	return 0;
+}
