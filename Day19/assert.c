8165d042a86f6003c94158016ff5c583ac739e86
toddhricik <toddhricik@gmail.com>
2021-08-31 07:19:37 -0400

    Listing 19.3

---
+++ Day19/assert.c
@@ -0,0 +1,12 @@
+// Listing 19.3 - Using the assert() macro
+#include <stdio.h>
+#include <assert.h>
+int main(void)
+{
+	int x;
+	printf("\nEnter an integer value: ");
+	scanf("%d", &x);
+	assert(x >= 0);
+	printf("You entered %d.\n", x);
+	return 0;
+}
