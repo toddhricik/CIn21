7071ec36bcc05ea7f4b91721a6f2879565e4f366
toddhricik <toddhricik@gmail.com>
2021-08-31 07:31:42 -0400

    Listing 19.4

---
+++ Day19/errno_and_perror.c
@@ -0,0 +1,23 @@
+// Listing 19.4 - Using errno and perror()
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+int main(void)
+{
+	FILE *fp;
+	char filename[80];
+	printf("Enter filename: ");
+	scanf("%c", filename);
+	if((fp = fopen(filename, "r")) == NULL)
+	{
+		perror("You goofed!");
+		printf("errno = %d.\n", errno);
+		exit(1);
+	}
+	else
+	{
+		puts("File opened for reading successfully.");
+		fclose(fp);
+	}
+	return 0;
+}
